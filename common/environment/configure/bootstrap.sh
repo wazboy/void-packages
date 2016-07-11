f4367bb0b59d2df5f5a9d3ad874ac66a3467b1cb
Galen Sampson <galen.sampson@gmail.com>
2016-07-10 20:30:36 -0700

    bootstrap env: set PKG_CONFIG_LIBDIR
    While bootstrapping pkg-config may not look in $XBPS_MASTERDIR for .pc
    files.  This may change some link options to leak from the host.  Set
    PKG_CONFIG_LIBDIR during bootstrap to get values from the build, not
    the host.

--- common/environment/configure/bootstrap.sh
+++ common/environment/configure/bootstrap.sh
@@ -1,4 +1,5 @@
 if [ -z "$CHROOT_READY" ]; then
 	CFLAGS+=" -isystem ${XBPS_MASTERDIR}/usr/include"
 	LDFLAGS+=" -L${XBPS_MASTERDIR}/usr/lib -Wl,-rpath-link=${XBPS_MASTERDIR}/usr/lib"
+	PKG_CONFIG_LIBDIR="${XBPS_MASTERDIR}/usr/lib/pkgconfig"
 fi
